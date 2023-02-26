4885fa75cceba328f5765135a53fabec7c93f4ad
Daniel <davilareko@gmail.com>
2023-02-26 23:29:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -190,6 +190,28 @@ function findASUSSupportUrl(productName, i, cb) {
     // return parsed;
 }
 
+function addReplaceAll() {
+    /**
+     * String.prototype.replaceAll() polyfill
+     * https://gomakethings.com/how-to-replace-a-section-of-a-string-with-another-one-with-vanilla-js/
+     * @author Chris Ferdinandi
+     * @license MIT
+     */
+    if (!String.prototype.replaceAll) {
+        String.prototype.replaceAll = function (str, newStr) {
+
+            // If a regex pattern
+            if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+                return this.replace(str, newStr);
+            }
+
+            // If a string
+            return this.replace(new RegExp(str, 'g'), newStr);
+
+        };
+    }
+}
+
 function simpleGET(url, cb) {
     let finalData;
     let chunks = [];
@@ -268,6 +290,8 @@ const adapterFor = (function () {
 }());
 
 const server = USE_HTTPS ? http.createServer({ key, cert }, requestHandler) : http.createServer(requestHandler);
+
+addReplaceAll();
 
 server.listen(port, (err) => {
     if (err) {
